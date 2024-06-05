20dee1d79c51da8136cd5af6d341da940c973ee1
Diego Alejandro Landaeta Torres <diegolandaeta@meta.com>
2024-06-05 16:43:49 -0700

    modal is uppppp

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,15 +1,18 @@
 // JavaScript for Opening and Closing the Modal
-var modal = document.getElementById("festivalModal");
+var modal = document.getElementById("playlistModal");
 var span = document.getElementsByClassName("close")[0];
 
-function openModal(festival) {
-   document.getElementById('festivalName').innerText = festival.name;
-   document.getElementById('festivalImage').src = festival.imageUrl;
-   document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
-   document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
-   document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
+function openModal(playlist) {
+   document.getElementById('playlistImage').src = playlist.imageUrl;
+   document.getElementById('playlistName').innerText = playlist.name;
+   document.getElementById('creator').innerText = playlist.creator;
+   // document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
+   // document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
+   // document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
    modal.style.display = "block";
 }
+
+
 
 span.onclick = function() {
    modal.style.display = "none";
