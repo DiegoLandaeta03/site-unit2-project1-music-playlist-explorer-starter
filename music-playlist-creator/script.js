2ce2985d9dce39d7852d3f8bb3e7568c67b61e0d
Diego Alejandro Landaeta Torres <diegolandaeta@meta.com>
2024-06-06 11:53:14 -0700

    modals and auto population works!!!

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -3,16 +3,14 @@ var modal = document.getElementById("playlistModal");
 var span = document.getElementsByClassName("close")[0];
 
 function openModal(playlist) {
-   document.getElementById('playlistImage').src = playlist.imageUrl;
-   document.getElementById('playlistName').innerText = playlist.name;
-   document.getElementById('creator').innerText = playlist.creator;
+   document.getElementById('playlistImage').src = playlist.playlist_art;
+   document.getElementById('playlistName').innerText = playlist.playlist_name;
+   document.getElementById('creator').innerText = playlist.playlist_creator;
    // document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
    // document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
    // document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
    modal.style.display = "block";
 }
-
-
 
 span.onclick = function() {
    modal.style.display = "none";
@@ -22,3 +20,36 @@ window.onclick = function(event) {
       modal.style.display = "none";
    }
 }
+
+
+function importPlaylists(){
+   // Display the fetched playlists
+   const playlistList = document.getElementById("playlists");
+   data.playlists.forEach((playlist) => {
+      let id = playlist.playlistID;
+      let name = playlist.playlist_name;
+      let creator = playlist.playlist_creator;
+      let art = playlist.playlist_art;
+      let likeCount = playlist.likeCount;
+      let songs = playlist.songs;
+      // console.log(playlist);
+      const listItem = document.createElement("div");
+      listItem.innerHTML = `
+         <div class="list">
+               <img class="image" src="${art}" alt="Playlist" width="200">
+               <h3>${name}</h3>
+               <p>${creator}</p>
+         </div>
+      `;
+
+      listItem.addEventListener("click", () => {
+         openModal(playlist);
+      });
+      playlistList.appendChild(listItem);
+   })
+}
+
+importPlaylists();
+
+
+
